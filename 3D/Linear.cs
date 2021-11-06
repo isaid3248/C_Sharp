fbd094f099bf52b14c1686c620919a07ff3840bc
isaid3248 <isaid3248@gmail.com>
2021-11-06 13:31:38 +0100

    C#

---
+++ 3D/Linear.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Linear : MonoBehaviour
+{ 
+	public Transform target;
+	void Start () {
+		
+	}
+	
+	
+	void Update () {
+	 
+		transform.position = Vector3.Lerp(transform.position, target.position, .2f); 
+	 // Lerp, linear interpolation yapar. Bu da hareketin kademeler halinde yapılmasını sağlar.
+	 // ( "Hangi noktadan gidilecek" , "Hangi noktaya gideceğim" , "Hangi oranlarda gideceğim")
+	 // 1f = %100 yani adım atmadan gider.
+	
+	 // transform.position = target.position;
+	
+	}
+	
+}
