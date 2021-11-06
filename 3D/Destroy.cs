6b9bd26f8154568c04e80562f07464e2f844a0f4
isaid3248 <isaid3248@gmail.com>
2021-11-06 18:24:55 +0100

    C#

---
+++ 3D/Destroy.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroy : MonoBehaviour
+{ 
+//	public GameObject Cube;  (Başka bir object üzerinden yok edeceksek bkz. 15, 16 )
+//  public ParticleSystem particleComponent;	(Direkt sadece component yok edeceksek bunu kullanabiliriz. Bkz. 19)
+	void Start () {
+		}
+	
+	void Update () {
+		if (Input.GetKey(KeyCode.Space))
+		{
+		    Destroy(GetComponent<ParticleSystem>);
+		 //	Destroy(Cube.gameObject);
+		 // Destroy(Cube.GetComponent<ParticleSystem>);
+		 //	Destroy(gameobject);
+		 // Destroy(particleComponent);
+		}
+	}
+	
+}
