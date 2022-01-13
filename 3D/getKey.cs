17ae4a9984a9fa5dd557e206692ca384ed660910
isaid3248 <isaid3248@gmail.com>
2022-01-13 20:09:28 +0100

    C#

---
+++ 3D/getKey.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class getKey : MonoBehaviour 
+{ 
+	void Start () {
+		
+	}
+	
+	
+	void Update () {
+		bool down = Input.GetKeyDown(KeyCode.Space);   // ilk basıldığı an
+		bool held = Input.GetKey(KeyCode.Space);   // Basılı tutulduğu zaman
+		bool up = Input.GetKeyUp(KeyCode.Space);   // Elini Çektiğin an
+
+		if (down) {
+			Debug.Log("Down");
+		}
+		else if (held) {
+			Debug.Log("Held");
+		}
+		else if (up) {
+			Debug.Log("Up");
+		}
+		else {
+			Debug.Log("Pas touché");
+		}
+		}
+	
+}
